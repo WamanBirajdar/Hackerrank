3394fefd7fb91287e448b9826831b2dc14eb6990
WAMAN BIRAJDAR <78862138+WamanBirajdar@users.noreply.github.com>
2023-01-04 22:35:39 +0530

    Update SetRemove_Discard_Pop.py

--- HackerRank/Python/SetRemove_Discard_Pop.py
+++ HackerRank/Python/SetRemove_Discard_Pop.py
@@ -11,3 +11,17 @@ s.remove(5)
 print(f'it will raise error: {s}')
 
 
+
+# it will also remove element form set
+# discard
+# if key not exist not raise error
+s = set([1, 2, 3, 4, 5, 6, 7, 8, 9])
+s.discard(5)
+print(s)
+
+s.discard(5)
+print(f'not raise key error {s}')
+
+
+
+
