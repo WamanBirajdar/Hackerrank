cee02a586e5b495421fadbd973ff5dd74b661aaf
WAMAN BIRAJDAR <78862138+WamanBirajdar@users.noreply.github.com>
2023-01-05 13:28:11 +0530

    Create itertools.py

---
+++ HackerRank/Python/itertools.py
@@ -0,0 +1,11 @@
+# itertools is combination of iterator and algebra
+# itertools is module work on iterator
+
+import itertools
+
+# for loop in 
+for i in itertools.count(5,5):
+    if i==35:
+        break
+    else:
+        print(i,end=" ")
